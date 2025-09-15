d6691ed64c530c58a4e83071f2523758104685c1
A345i <82287724+A345i@users.noreply.github.com>
2025-09-15 13:30:20 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, err => {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     try {
         // --- Supabase Setup ---
